757e7f7d90cfec18ea01c5a9ec2aa33cd9dc862c
JonasHalver <44400498+JonasHalver@users.noreply.github.com>
2020-07-12 11:52:22 +0200

    New Version

---
+++ Assets/MoveController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float movementSpeed = 2f, sprintModifier = 1.5f, aimModifier = 0.75f, speedModifier;
+    Vector3 movementVector;
+    private Transform cam;
+
+    public AudioSource sounds;
+    public List<AudioClip> steps = new List<AudioClip>();
+    float t = 0;
+
+    public Animator anim;
+
+    bool sprinting;
+    bool aiming;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = transform.GetChild(0).GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.ResetTrigger("Shoot");
+        aiming = Input.GetMouseButton(1);
+        sprinting = !aiming && Input.GetButton("Sprint");
+        speedModifier = Input.GetMouseButton(1) ? aimModifier : (Input.GetButton("Sprint") ? sprintModifier : 1);
+        movementVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        TranslateMovement(CameraForward(movementVector));
+        if (movementVector != Vector3.zero)
+        {
+            anim.SetFloat("IdleWalk", 1f);
+            Footsteps();
+
+            if (sprinting)
+                anim.SetFloat("WalkSprint", 1f);
+            else
+                anim.SetFloat("WalkSprint", 0f);
+
+        }
+        else
+        {
+            anim.SetFloat("IdleWalk", 0f);
+        }
+
+        anim.SetBool("Aiming", aiming);
+
+        if (Input.GetButtonDown("Fire1"))
+            anim.SetTrigger("Shoot");
+
+    }
+
+    Vector3 CameraForward(Vector3 input)
+    {
+        Vector3 output = Vector3.zero;
+        output += cam.forward * input.z * movementSpeed * speedModifier;
+        output += cam.right * input.x * movementSpeed * 0.75f;
+        return output;
+    }
+
+    public void TranslateMovement(Vector3 input)
+    {
+        rb.velocity = new Vector3(input.x, rb.velocity.y, input.z);
+    }
+
+    public void Footsteps()
+    {
+        if (t == 0)
+        {
+            sounds.PlayOneShot(steps[Random.Range(0, 6)]);
+        }
+        t += Time.deltaTime;
+        float stepDelay = (2 - speedModifier)/2;
+        if (t >= stepDelay)
+        {
+            t = 0;
+        }
+    }
+}
